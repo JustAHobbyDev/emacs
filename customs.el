22e0e1ced9a813cc2499d7e622e3739463730da0
Daniel Schmidt <jahahobbydev@proton.me>
2024-01-29 22:58:46 -0600

    My emacs configuration.
    Based on evil-mode at this time. Save this before trying meow-mode.

---
+++ customs.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1d2e7f3afdd436cf7b1f7d009111e9890328da1f68380c71ad8041ebd62a0a95" default))
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ early-init.el
@@ -0,0 +1,4 @@
+;; -*- lexical-binding: t -*-
+
+;; Elpaca requires package.el to be disabled
+(setq package-enable-at-startup nil)
